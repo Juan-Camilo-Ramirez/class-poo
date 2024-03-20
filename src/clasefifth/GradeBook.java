c1cced44bb0ac80f55700f61f43cd8638e86954a
Juan Camilo <juan.ramirez44687@ucaldas.edu.co>
2024-03-20 15:36:04 -0500

    gradebook

--- src/clasefifth/GradeBook.java
+++ src/clasefifth/GradeBook.java
@@ -1,2 +1,18 @@
-package clasefifth;public class GradeBook {
+package clasefifth;
+
+public class GradeBook {
+    private String courseName;
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName=courseName;
+    }
+
+    public GradeBook(){}
+    public String displayMessage(){
+        return "welcome to the gradebook, course name is: " + getCourseName();
+    }
+
 }
--- src/clasefifth/GradeBookTest.java
+++ src/clasefifth/GradeBookTest.java
@@ -1,2 +1,17 @@
-package clasefifth;public class GradeBookTest {
+package clasefifth;
+
+public class GradeBookTest {
+    public static void main(String[] args) {
+        GradeBook planilla = new GradeBook();
+        planilla.setCourseName("CS101 INTRODUCTION TO JAVA PROGRAMMING");
+        GradeBook planilla1 = new GradeBook();
+        planilla1.setCourseName("CS102 DATA STRUCTURES IN JAVA");
+
+        String message = planilla.displayMessage();
+        System.out.println(message);
+        String message1 = planilla1.displayMessage();
+        System.out.println(message1);
+    }
 }
+
+
